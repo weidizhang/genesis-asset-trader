47b964ab92e36d6c87547bdb88416112e3fe32a6
Weidi Zhang <weidiz999@yahoo.com>
2019-12-28 20:32:29 -0800

    Properly remove "bad" extremas in predictions

--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
 from joblib import load
 
 class Predict:
-    def __init__(self, model_file, k_neighbors = 5, max_conflicts = 0, search_distance = 24): # remove default values
+    def __init__(self, model_file, k_neighbors = 4, max_conflicts = 0, search_distance = 24): # TODO: remove default values?
         self.load_model(model_file)
         self.set_k_neighbors(k_neighbors)
         self.set_search_distance(search_distance)
@@ -18,23 +18,21 @@ class Predict:
         df_features["Extrema"] = predict
         return df_features
 
+    # Returns number of removed extremas from the originally predicted data based on
+    # the decision tree model
+    #
+    # Note that the dataframe df is changed in place with predicted extrema results
     def predict_full(self, df, df_features, model_predict_only = False):
         model_predicted = self.predict_with_model(df_features)
         self._merge_data(df, model_predicted)
 
         if model_predict_only:
-            return df # TODO: ??
-
-        # for each extrema, verify them with additional rules
+            return 0
         
         bad_extremas = self.validate_extremas(df)
-        removed = len(list(bad_extremas))
+        df.loc[bad_extremas, "Extrema"] = 0
 
-        print("Removed extremas:", removed)
-        # just conflicts: 206
-        # with k neighbors: 
-
-        return df
+        return len(bad_extremas)
 
     def set_k_neighbors(self, k):
         self._k = k
@@ -46,12 +44,14 @@ class Predict:
     def set_max_conflicts(self, max_conflicts):
         self._max_conflicts = max_conflicts
 
-    # Yields indices of extremas that should be deleted due to having conflicts or not
+    # Returns list of indices of extremas that should be deleted due to having conflicts or not
     # having the minimum k neighbors of same extrema type as predicted by the model
     #
-    # A generator is used as we want to search the df in its unmodified state for
-    # our heuristic checks
+    # We want to search df in its unmodified state for our heuristic checks, rather than
+    # modifying extrema data in place while still searching through the data frame
     def validate_extremas(self, df):
+        bad_indices = []
+
         for i, row in df.iterrows():
             extrema_type = row["Extrema"]
             if extrema_type == 0:
@@ -59,7 +59,9 @@ class Predict:
 
             search_region = df.iloc[i - self._distance:i]
             if self._has_conflicts(search_region, extrema_type) or not self._has_k_neighbors(search_region, extrema_type):
-                yield i
+                bad_indices.append(i)
+
+        return bad_indices
 
     def _has_conflicts(self, region, extrema_type):
         # Note that (extrema_type * -1) is the opposite extrema type, where 1 = maxima, -1 = minima
